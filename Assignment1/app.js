a590906f070bc1aae5766fab417aae2ec86c8e35
sonkar-amit <sonkar.amit08@gmail.com>
2016-12-09 20:25:02 +0530

    JS Assignment1

---
+++ Assignment1/app.js
@@ -0,0 +1,9 @@
+var name="Amit";
+var age = 27;
+var dateOfBirth = "23-April";
+var placeOfBirth = "Kolkata";
+
+console.log(name);
+console.log(age);
+console.log(dateOfBirth);
+console.log(placeOfBirth);
